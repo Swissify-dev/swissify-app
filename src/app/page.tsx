0a9e02914d7fa9f2e96718d5a2cab43782293163
MaidedPonce <maided.h.p90@gmail.com>
2024-05-29 20:48:58 -0600

    Adding fix to component types

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import { Welcome } from '@components/containers/Welcome'
+import { FC } from 'react'
 
-export default function Home() {
+const Home: FC = () => {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <Welcome />
     </main>
   )
 }
+
+export default Home
--- src/components/containers/Welcome.tsx
+++ src/components/containers/Welcome.tsx
@@ -1,11 +1,12 @@
 'use client'
-import { useMutation, useQuery } from '@tanstack/react-query'
+import React, { FC } from 'react'
+import { useMutation } from '@tanstack/react-query'
 import { hello as helloFn } from 'src/services/hello'
 import { Button } from '../ui/button'
 import { AxiosError } from 'axios'
 import { useAppConfig } from 'src/store/app-config'
 
-const Welcome = () => {
+const Welcome: FC = () => {
   const setLanguage = useAppConfig((state) => state.setLanguage)
   const {
     data,
